07e6e5c548b4ca5d1cc1be8a0e2cd3fb470044ce
nicola <sosio.nicola94@tiscali.it>
2017-11-22 19:22:24 +0100

    Fix bootstrap

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', index); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/fonts/', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/fonts'))); //redirect bootstrap glyphicons
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
