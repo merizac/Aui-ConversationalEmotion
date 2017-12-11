8bc2a82314f838c61ba049f9f96b47d5c07308f3
nicola <sosio.nicola94@tiscali.it>
2017-12-11 18:48:10 +0100

    Added variable user to the session

--- routes/session1.js
+++ routes/session1.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('session1', { title: 'Speech Sample' });
+  res.render('session1', { user: 'Giovanni' });
 });
 
 module.exports = router;
