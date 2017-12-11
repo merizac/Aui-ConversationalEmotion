00892fd88dbb16e8c6f256743de5b9037addc0c9
nicola <sosio.nicola94@tiscali.it>
2017-12-11 18:49:29 +0100

    Modify

--- routes/session1.js
+++ routes/session1.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('session1', { user: 'Giovanni' });
+  res.render('session1');
 });
 
 module.exports = router;
